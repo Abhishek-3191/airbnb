baeae102a9cb44156435ee0cb5d0db57e5f661bb
Abhishek-3191 <abhisheksrivastava49631@gmail.com>
2025-03-08 21:58:22 +0530

    Hashing,salting,authentication and authorization

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
-const listings=require("./routes/listing.js");
-const reviews=require("./routes/review.js")
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
+
 const session=require("express-session")
 const flash=require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
+
+
 
 async function main() {
 await mongoose.connect(MONGO_URL);
@@ -51,6 +58,16 @@ app.use(session(sessionOptions));
 
 app.use(flash());
 //flash is a middleware
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
@@ -58,12 +75,20 @@ app.use((req,res,next)=>{
 })
 
 
+app.get("/demouser",async(req,res)=>{
+    let fakeUser=new User({
+        email:"student@gmail.com",
+        username:"delta-student"
+    });
+  let registeredUser= await User.register(fakeUser,"helloWorld");
+  res.send(registeredUser);
+});
 
 
 
-
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 
 
