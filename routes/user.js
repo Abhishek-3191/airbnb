58b50898242dcb86f7916c3d8e0629bd6936685b
Abhishek-3191 <abhisheksrivastava49631@gmail.com>
2025-03-14 00:51:45 +0530

    modifying the signup

--- routes/user.js
+++ routes/user.js
@@ -13,8 +13,14 @@ router.post("/signup",wrapAsync(async(req,res)=>{
     const {username,email,password}=req.body;
     const newUser=new User({email,username});
     let registeredUser=await User.register(newUser,password);
-    req.flash("success","Welcome to WanderLust");
-    res.redirect("/listings")
+
+    req.login(registeredUser,(err)=>{
+      if(err){
+        return next(err);
+      }
+          req.flash("success","Welcome to WanderLust");
+          res.redirect("/listings")
+    });
     } catch (error) {
         req.flash("error",error.message);
         res.redirect("/signup")
