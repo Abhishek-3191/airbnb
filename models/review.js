b496e6b572058a8dba14f8a61174c748e676e83e
Abhishek-3191 <abhisheksrivastava49631@gmail.com>
2025-03-18 16:41:54 +0530

    authentication and authorization continued

--- models/review.js
+++ models/review.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
+const User=require("./user.js");
 
 const reviewSchema=new Schema({
     comment:String,
@@ -12,5 +13,9 @@ const reviewSchema=new Schema({
         type:Date,
         default:Date.now(),
     },
+    author:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+    }
 });
 module.exports=mongoose.model("Review",reviewSchema);
--- routes/review.js
+++ routes/review.js
@@ -1,19 +1,17 @@
 const express=require('express');
 const router=express.Router({mergeParams:true});
 const wrapAsync=require("../utils/wrapAsync.js");
-const ExpressError=require("../utils/ExpressError.js");
-
 const Review=require("../models/review.js");
 const Listing=require("../models/listing.js");
-const {validateReview}=require("../middleware.js");
+const {validateReview,isLoggedIn,isReviewAuthor}=require("../middleware.js");
 
 
 //reviews
 //post
-router.post("/",validateReview,wrapAsync(async(req,res)=>{
+router.post("/",isLoggedIn,validateReview,wrapAsync(async(req,res)=>{
     let listing=await Listing.findById(req.params.id);
     let newReview=new Review(req.body.review);
-
+    newReview.author=req.user._id;
     listing.reviews.push(newReview)
 
     await newReview.save()
@@ -25,12 +23,12 @@ return res.redirect(`/listings/${listing._id}`);
 }));
 
 //Delete route for review
-router.delete("/:reviewId",wrapAsync(async(req,res)=>{
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(async(req,res)=>{
     let {id,reviewId}=req.params;
 
     await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
     await Review.findByIdAndDelete(reviewId);
-    req.flash("success","New Listing deleted");
+    req.flash("success","Review deleted");
     res.redirect(`/listings/${id}`);
 }));
 
